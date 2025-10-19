b7f13c9e01ab4adf6c1b4da79227d39468ba9e54
amir-mansoor <qazimansor89@gmail.com>
2025-10-19 11:30:28 +0500

    logical and relational operators

---
+++ relational_and_logical_expressions.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+// int main() {
+//     int a = true;
+//     int b = false;
+
+//     cout << "a && b: " << (a && b) << endl;
+//     cout << "a || b: " << (a || b) << endl;
+//     cout << "!a: " << (!a) << endl;
+//     return 0;
+// }
+
+// relation ship 
+
+int main() {
+    cout << "3 > 4: " << (3 > 4) << endl;
+    return 0;
+}
