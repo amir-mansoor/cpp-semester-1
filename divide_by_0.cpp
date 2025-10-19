d69ecbbe8a0ddd8d016a780828484ac19ff8f305
amir-mansoor <qazimansor89@gmail.com>
2025-10-19 10:29:28 +0500

    divide by 0 - runtime error

---
+++ divide_by_0.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int divide = 0;
+    cout << "Enter number to divide" << endl;
+    cin >> divide;
+    cout << " " << divide/2;
+    return 0;
+}
